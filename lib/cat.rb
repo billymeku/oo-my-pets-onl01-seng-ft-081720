946d26ff8bd066c9fbe82fb601206efebaa51422
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-02 05:27:56 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,10 @@
 class Cat
   # code goes here
+  attr_accessor :owners 
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,48 @@
 class Owner
-  # code goes here
+ 
+ # code goes here
+attr_accessor:cats, :dogs 
+ attr_reader :name, :speices  
+ @@owners = []
+
+ #  Instance methods
+ 
+  def initialize(name)
+    @name = name 
+    @speices = "human"
+      @@owners << self
+    @cats = []
+    @dogs = []
+  
+  end
+  
+  def say_species()
+    
+    "This is a #{speices}"
+    
+   end
+   
+   def cats
+     
+     @cats
+   end 
+   
+   def dogs
+     
+     @dogs
+   end    
+  
+  def self.all
+    @@owners << self 
+   end
+   
+  def self.count 
+    @@owners.count 
+    
+  end 
+  
+  def self.reset_all
+    @@owners.clear 
+  end 
+
 end
