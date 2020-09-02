3b09b9eb833b052c8afa9a6480f5c8a1a04994b3
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-02 05:34:49 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,49 +1,5 @@
 class Owner
  
- # code goes here
-# attr_accessor:cats, :dogs 
-# attr_reader :name, :speices  
-# @@owners = []
-
-# #  Instance methods
- 
-#   def initialize(name)
-#     @name = name 
-#     @speices = "human"
-#       @@owners << self
-#     @cats = []
-#     @dogs = []
-  
-#   end
-  
-#   def say_species()
-    
-#     "This is a #{speices}"
-    
-#   end
-   
-#   def cats
-     
-#     @cats
-#   end 
-   
-#   def dogs
-     
-#     @dogs
-#   end    
-  
-#   def self.all
-#     @@owners << self 
-#   end
-   
-#   def self.count 
-#     @@owners.count 
-    
-#   end 
-  
-#   def self.reset_all
-#     @@owners.clear 
-#   end 
  attr_accessor :cats, :dogs
   attr_reader :name, :species
 
@@ -113,11 +69,11 @@ class Owner
   end
 
   def self.count()
-    @@owners.count
+    @@owners.size 
   end
 
   def self.reset_all()
-    @@owners = []
+    @@owners.clear
   end
 
 end
